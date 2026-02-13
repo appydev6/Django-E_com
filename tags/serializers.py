60d075c58d5b7a4d6d3db04faa82ca6cfa04eb5c
appydev6 <ashwinpahade97@gmail.com>
2026-02-13 21:07:41 +0530

    created tag api

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from tags.models import Tags
 
+# types of serializer by name
+# read serilizer is used to read operations of DB
+# write serilizer is user to write operations of DB
+
 class WriteTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tags
-        fields = ('name', 'slug')
+        fields = ('name',)
+        # Single-item tuple (needs trailing comma) 
+        # because The `fields` option must be a list or tuple or "__all__".
 
 class ReadTagSerializer(serializers.ModelSerializer):
     class Meta:
--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
+from tags.serializers import WriteTagSerializer, ReadTagSerializer
+from tags.models import Tags
+from django.utils.text import slugify
 
-# Create your views here.
+class CreateTagView(views.APIView):
+    def post(self, request):
+        serializer = WriteTagSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            #'validated_data' has to be called after the 'is_valid' only 
+            # (or) else it will if it is called directly it will through an error.
+            tag_object = Tags.objects.create(name=name, slug=slugify(name))
+            response_data = ReadTagSerializer(instance=tag_object).data
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
