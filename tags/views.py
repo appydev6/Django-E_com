659e81296df8761de53159051a22d21d1affb5a9
appydev6 <ashwinpahade97@gmail.com>
2026-02-16 09:55:34 +0530

    delete api view added

--- tags/views.py
+++ tags/views.py
@@ -3,15 +3,16 @@ from rest_framework.response import Response
 from tags.serializers import WriteTagSerializer, ReadTagSerializer
 from tags.models import Tags
 from django.utils.text import slugify
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import RetrieveAPIView, DestroyAPIView
+from rest_framework.views import APIView
 
-class CreateTagView(views.APIView):
+class CreateTagView(APIView):
     def post(self, request):
         serializer = WriteTagSerializer(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             #'validated_data' has to be called after the 'is_valid' only 
-            # (or) else it will if it is called directly it will through an error.
+            # (or) else if it is called directly it will through an error.
             tag_object = Tags.objects.create(name=name, slug=slugify(name))
             response_data = ReadTagSerializer(instance=tag_object).data
             return Response(response_data, status=status.HTTP_200_OK)
@@ -19,7 +20,7 @@ class CreateTagView(views.APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class TagDetailViewV1(views.APIView):
+class TagDetailViewV1(APIView):
     def get(self, request, slug):
         try:
             tag_object = Tags.objects.get(slug=slug)
@@ -31,4 +32,18 @@ class TagDetailViewV1(views.APIView):
 class TagDetailViewV2(RetrieveAPIView):
     queryset = Tags.objects.all()
     serializer_class = ReadTagSerializer
+    lookup_field = "slug"
+
+class DeleteTagView1(APIView):
+    
+    def delete(self, request, slug):
+        try:
+            tag_object = Tags.objects.get(slug=slug)
+            tag_object.delete()
+            return Response({"message" : "Tag deleted"}, status=status.HTTP_200_OK)
+        except (Tags.DoesNotExist, Tags.MultipleObjectsReturned):
+            return Response({"message" : "Tag does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+    
+class DeleteTagView2(DestroyAPIView):
+    queryset = Tags.objects.all()
     lookup_field = "slug"
